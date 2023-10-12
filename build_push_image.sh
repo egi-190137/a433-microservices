bced98e114c5f5f9edaec0b0d1fdb8a15cb36b07
egi-190137 <egiputra450@gmail>
2023-10-12 15:09:03 +0700

    commit

---
+++ build_push_image.sh
@@ -0,0 +1,10 @@
+# build image dengan nama item-app
+docker build . -t item-app:v1
+# Menampilkan daftar image
+docker images
+# memberikan tag pada image agar dapat di-push
+docker tag item-app:v1 ghcr.io/egi-190137/item-app:v1
+# login pada github container registry dengan token yang tersimpan pada environment variable
+echo $GH_TOKEN | sudo docker login ghcr.io -u egi-190137 --password-stdin
+# Push image ke github container registry
+sudo docker push ghcr.io/egi-190137/item-app:v1
