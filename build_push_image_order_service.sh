a7acadecda57428cb1cd0831dfdc166d7ca2634b
egi-190137 <egiputra450@gmail>
2023-11-18 15:10:12 +0700

    update build script

--- build_push_image_order_service.sh
+++ build_push_image_order_service.sh
@@ -1,4 +1,4 @@
-# build image dengan nama ghcr.io/<username>/karsajobs-ui:latest
+# build image dengan nama ghcr.io/<username>/order-service:latest
 docker build -t ghcr.io/egi-190137/order-service:latest .
 # login pada github container registry dengan token yang tersimpan pada environment variable
 echo $GH_TOKEN | sudo docker login ghcr.io -u egi-190137 --password-stdin
