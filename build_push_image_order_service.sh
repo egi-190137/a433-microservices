d16ea55e0f68b399d9884cdf7b418154a0a89909
egi-190137 <egiputra450@gmail>
2023-11-18 15:05:58 +0700

    add script for push image to ghcr.io

---
+++ build_push_image_order_service.sh
@@ -0,0 +1,6 @@
+# build image dengan nama ghcr.io/<username>/karsajobs-ui:latest
+docker build -t ghcr.io/egi-190137/order-service:latest .
+# login pada github container registry dengan token yang tersimpan pada environment variable
+echo $GH_TOKEN | sudo docker login ghcr.io -u egi-190137 --password-stdin
+# Push image ke github container registry
+sudo docker push ghcr.io/egi-190137/order-service:latest
